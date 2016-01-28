9b7fa7f0a9f722b714bee424675ecb5c32b7d8ba
Luna van der Loos <lunavdloos@gmail.com>
2016-01-28 14:48:18 +0000

    Update treemaker.sh

--- Scripts/treemaker.sh
+++ Scripts/treemaker.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-# This script prints a description of the original sequences, does a multiple alignment, 
-# estimates the best phylogenetic tree and displays this tree
+# This script does a multiple alignment (output = phylip format) and estimates the best phylogenetic tree
 
 # $1 is the fasta file input $2 is the bootstrapvalue
 # input in terminal: 'Treemaker filename bootstrapvalue'
 # default bootstrap value = 20
-# the name of the output value can be changed in "-outfile= " (line 13)
+# the name of the output value can be changed in "-outfile= " (line 12)
 
 Treemaker(){
 	# align using Clustal-W	
